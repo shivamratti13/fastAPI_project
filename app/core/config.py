c0b9f5b7453189e82c0203477a42bf40c9630df8
Shivam Ratti <82520512+shivamratti13@users.noreply.github.com>
2025-08-31 18:27:52 +0530

    final changes

--- app/core/config.py
+++ app/core/config.py
@@ -3,13 +3,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-class Settings:
 
-    PROJECT_NAME = 'Car Price'
-    API_KEY = os.getenv('API_KEY', 'demo_key')
-    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY','secret')
+class Settings:
+    PROJECT_NAME = 'Car Price API'
+    API_KEY = os.getenv('API_KEY', 'demo-key')
+    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'secret')
     JWT_ALGORITHM = 'HS256'
-    REDIS_URL = os.getenv('REDIS_URL')
-    MODEL_PATH = 'app/model/model.pkl'
+    REDIS_URL = os.getenv('REDIS_URL', 'redis://localhost:6379')
+    MODEL_PATH = 'app/models/model.joblib'
+
 
 settings = Settings()
--- app/main.py
+++ app/main.py
@@ -1,17 +1,17 @@
 from fastapi import FastAPI
 from prometheus_fastapi_instrumentator import Instrumentator
-from app.api import routes_auth, route_predict
+from app.api import routes_auth, routes_predict
 from app.middleware.logging_middleware import LoggingMiddleware
 from app.core.exceptions import register_exception_handlers
 
-app = FastAPI(title='car price prediction api')
+app = FastAPI(title='Car Price Prediction API')
 
 # link middleware
 app.add_middleware(LoggingMiddleware)
 
 # link endpoints
 app.include_router(routes_auth.router, tags=['Auth'])
-app.include_router(route_predict, tags=['Prediction'])
+app.include_router(routes_predict.router, tags=['Prediction'])
 
 # monitoring using Prometheus
 Instrumentator().instrument(app).expose(app)
